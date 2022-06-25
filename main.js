b6cdddbf319fd22822abff12b831a33149a588b5
RukuWasTaken <something0879@gmail.com>
2022-06-25 19:21:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,34 +20,15 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 }
 
-function createMainMenu(){
-  const template = [
-    {
-      label: "Lists",
-      submenu: [
-        {
-          label: "Create new list",
-          accelerator: "CommandOrControl+N",
-          click() {
-            mainWindow.webContents.send("create-list");
-          }
-        }
-      ]
-    }
-  ];
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
-}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  createMainMenu()
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();createMainMenu()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
