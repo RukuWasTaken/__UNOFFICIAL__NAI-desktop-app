5aafaa0a0b8e6f1300e3a9484926501a45c87c9a
RukuWasTaken <something0879@gmail.com>
2022-06-26 14:01:27 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 @echo This should auto-path-find.
 @echo off
-call git pull __UNOFFICIAL__NAI-desktop-app master
+call git pull https://github.com/RukuWasTaken/__UNOFFICIAL__NAI-desktop-app
 call npm install
 exit
